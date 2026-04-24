86e3fa6c7c0deb770a433ab9429fdbae98f51c56
Kai Walter <kai.walter@zeiss.com>
2026-04-24 07:32:27 +0200

    initial version

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Pi coding agent dev environment with Node.js 24";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            _1password-cli
+            jq
+          ];
+
+          shellHook = ''
+            unset http_proxy
+            unset https_proxy
+            unset no_proxy
+
+            export NPM_CONFIG_PREFIX=~/.npm-global
+            export PATH=~/.npm-global/bin:$PATH
+            mkdir -p ~/.npm-global
+
+            op account get --account my
+            if [ $? -ne 0 ]; then
+              eval $(op signin --account my)
+            fi
+
+            export AZURE_OPENAI_API_KEY=$(op item get "Azure OpenAI API Key" --vault Private --fields label=key --format json | jq -r '.value')
+            export AZURE_OPENAI_BASE_URL="$(op item get "Azure OpenAI API Key" --vault Private --fields label=url --format json | jq -r '.value')/openai/v1"
+
+            if ! npm list -g @mariozechner/pi-coding-agent@0.70.0 > /dev/null 2>&1; then
+              npm install -g @mariozechner/pi-coding-agent@0.70.0
+            fi
+
+            exec pi
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+      }
+    );
+}
